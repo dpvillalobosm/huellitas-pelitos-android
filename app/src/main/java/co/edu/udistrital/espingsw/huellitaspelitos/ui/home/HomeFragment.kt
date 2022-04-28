cf2108def3cf8b8f411ee01c185bad461f33aa60
Camilo Andrés Díaz Serrano <camilodiaz369@gmail.com>
2022-04-28 18:21:50 -0500

    feat: ViewPager

--- app/src/main/java/co/edu/udistrital/espingsw/huellitaspelitos/ui/home/HomeFragment.kt
+++ app/src/main/java/co/edu/udistrital/espingsw/huellitaspelitos/ui/home/HomeFragment.kt
@@ -1,12 +1,15 @@
 package co.edu.udistrital.espingsw.huellitaspelitos.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.viewpager2.widget.ViewPager2
+import co.edu.udistrital.espingsw.huellitaspelitos.R
 import co.edu.udistrital.espingsw.huellitaspelitos.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -16,6 +19,8 @@ class HomeFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private val images = intArrayOf(R.drawable.a1, R.drawable.a2, R.drawable.a3, R.drawable.a4,
+        R.drawable.a5, R.drawable.a6, R.drawable.a7, R.drawable.a8)
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,15 +33,24 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
         homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+            Log.d(TAG, it)
         }
+
+        val mViewPager = binding.viewPager
+        val mViewPagerAdapter = context?.let { ViewPagerAdapter(it, images) }
+
+        mViewPager.adapter = mViewPagerAdapter
+
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object {
+        private const val TAG = "HomeFragment"
     }
 }
